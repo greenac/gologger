6612bf08bc7e6190f7859b9b6ac3fabf02ec8497
greenac <andre1226.dev@gmail.com>
2017-05-28 12:39:34 +0100

    initial commit

---
+++ logger.go
@@ -0,0 +1,55 @@
+package logger
+
+import (
+	"fmt"
+	"time"
+	"os"
+)
+
+type logger struct {
+	LogLevel int
+	LogPath string
+	timeFormat string
+	isSetup bool
+}
+
+func (l *logger)setup() {
+	if l.timeFormat == "" {
+		l.timeFormat = time.UnixDate
+	}
+
+	l.isSetup = true
+}
+
+func (l *logger)log(a ...interface{}) {
+	args := fmt.Sprint(a)
+	msg := time.Now().Format(time.UnixDate) + " :: " + args[1: len(args) - 1]
+	fmt.Println(msg)
+
+	// TODO: Add support for different log levels
+	if l.LogLevel == 1 {
+		go func(message string) {
+			f, err:= os.OpenFile(l.LogPath, os.O_APPEND | os.O_WRONLY | os.O_CREATE, 0644)
+			if err != nil {
+				fmt.Println("Error: could not write to octopus log file:", l.LogPath)
+				return
+			}
+			defer f.Close()
+
+			message += "\n"
+			_, err = f.WriteString(message)
+			if err != nil {
+				fmt.Println("Error: failed to write message to log file:", l.LogPath)
+			}
+		}(msg)
+	}
+}
+
+var l logger;
+func Log(a ...interface{}) {
+	if !l.isSetup {
+		l.setup()
+	}
+
+	l.log(a)
+}
